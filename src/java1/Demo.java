3c1e8d3eb2e8764a03ff7066902d9e672bdfacc5
kiruthikcs <kiruthika.a.k@capgemini.com>
2017-06-05 18:04:56 +0530

    rd

---
+++ src/java1/Demo.java
@@ -0,0 +1,13 @@
+package java1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	int a=10;
+	int b=20;
+		int c=a+b;
+	System.out.print("addition of a & b num is "+c);
+
+	}
+
+}
